f800303bd1a5257b3346e5c96baef76b086b52a3
William  Lin <willlin1996@gmail.com.com>
2025-03-31 13:51:40 -0400

    changed build.sh

--- build.sh
+++ build.sh
@@ -5,8 +5,3 @@ set -o errexit
 # Install dependencies
 bundle install
 
-# Run database migrations
-bundle exec rails db:migrate
-
-# Precompile assets for production
-bundle exec rails assets:precompile
